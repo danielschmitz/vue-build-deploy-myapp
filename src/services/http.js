32c74ad7ea05aa909704cd3b998fd1e8b2acb94d
danielschmitz <danieljfa@gmail.com>
2018-09-20 06:57:36 -0300

    .

--- src/services/http.js
+++ src/services/http.js
@@ -1,9 +1,34 @@
 import axios from 'axios';
+import store from '../store';
+import router from '../router';
 
 const http = axios.create({
-    baseURL: process.env.VUE_APP_ROOT_API,
-    timeout: 10000,
-    headers: { 'Content-Type': 'application/json' },
+  baseURL: process.env.VUE_APP_ROOT_API,
+  timeout: 10000,
+  headers: { 'Content-Type': 'application/json' },
 });
+
+http.interceptors.request.use(
+  config => {
+    const token = store.getters.getToken;
+    if (token) config.headers.Authorization = `Bearer ${token}`;
+    return config;
+  },
+  error => Promise.reject(error),
+);
+
+http.interceptors.response.use(
+  response => {
+    return response;
+  },
+  error => {
+    if (error.response !== undefined) {
+      if (error.response.status === 401) {
+        router.push('/login');
+      }
+    }
+    return Promise.reject(error);
+  },
+);
 
 export default http;
