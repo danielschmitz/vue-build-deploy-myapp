01b5a742bfdceec0bcabb454e09a80ef73a83af8
Daniel Schmitz <danieljfa@gmail.com>
2018-11-14 09:03:54 -0300

    webpack alias @
    https://github.com/Microsoft/vscode/issues/16320

--- src/views/Customers.vue
+++ src/views/Customers.vue
@@ -7,8 +7,7 @@
             </v-card-title>
             <v-card-text>
                 <v-text-field type="text" v-model="search" append-icon="search" label="Search" @click:append="onSearchClick"></v-text-field>
-                <v-data-table :headers="headers" :items="customers" :pagination.sync="pagination" :total-items="total"
-                    :loading="loading" class="elevation-1">
+                <v-data-table :headers="headers" :items="customers" :pagination.sync="pagination" :total-items="total" :loading="loading" class="elevation-1">
                     <template slot="items" slot-scope="props">
                         <td>{{ props.item.companyName }}</td>
                         <td>{{ props.item.contactName }}</td>
@@ -25,50 +24,50 @@
     </v-container>
 </template>
 <script>
-    import customer from '@/services/customer';
+import customer from '@/services/customer';
 
-    export default {
-        name: 'Customers',
-        data() {
-            return {
-                customers: [],
-                total: 0,
-                search: '',
-                pagination: {},
-                loading: false,
-                headers: [
-                    { text: 'Company Name', value: 'companyName', sortable: false, },
-                    { text: 'Contact Name', value: 'contactName', sortable: false, },
-                    { text: 'Contact Title', value: 'contactTitle', sortable: false, },
-                ]
-            }
-        },
-        mounted() {
-            console.log('Customers Mounted');
-        },
-        watch: {
-            pagination: {
-                handler() {
-                    this.getCustomers()
-                },
-                deep: true
-            }
-        },
-        methods: {
-            getCustomers() {
-                const { page, rowsPerPage } = this.pagination
-                let start = (page - 1) * rowsPerPage
-                let end = page * rowsPerPage
-                this.loading = true
-                customer.getByRange(start, end, this.search).then(r => {
-                    this.loading = false
-                    this.total = parseInt(r.headers['x-total-count'])
-                    this.customers = r.data
-                })
-            },
-            onSearchClick() {
+export default {
+    name: 'Customers',
+    data () {
+        return {
+            customers: [],
+            total: 0,
+            search: '',
+            pagination: {},
+            loading: false,
+            headers: [
+                { text: 'Company Name', value: 'companyName', sortable: false, },
+                { text: 'Contact Name', value: 'contactName', sortable: false, },
+                { text: 'Contact Title', value: 'contactTitle', sortable: false, },
+            ]
+        }
+    },
+    mounted () {
+        console.log('Customers Mounted');
+    },
+    watch: {
+        pagination: {
+            handler () {
                 this.getCustomers()
-            }
+            },
+            deep: true
+        }
+    },
+    methods: {
+        getCustomers () {
+            const { page, rowsPerPage } = this.pagination
+            let start = (page - 1) * rowsPerPage
+            let end = page * rowsPerPage
+            this.loading = true
+            customer.getByRange(start, end, this.search).then(r => {
+                this.loading = false
+                this.total = parseInt(r.headers['x-total-count'])
+                this.customers = r.data
+            })
+        },
+        onSearchClick () {
+            this.getCustomers()
         }
     }
+}
 </script>
