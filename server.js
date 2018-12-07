3382778997aff09c050cc099c43dd9207e4e591c
danielschmitz <danieljfa@gmail.com>
2018-12-07 19:17:04 -0200

    static server

---
+++ server.js
@@ -0,0 +1,7 @@
+var nodestatic = require('node-static');
+var file = new nodestatic.Server('./dist');
+require('http').createServer(function(request, response) {
+  request.addListener('end', function() {
+    file.serve(request, response);
+  }).resume();
+}).listen(process.env.PORT || 3000);
